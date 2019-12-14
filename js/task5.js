4e57e0d8ff8ac487d029d7f9a5a96bc769c2c634
Alexandr <e7000@ukr.net>
2019-12-14 21:45:21 +0200

    hw2

---
+++ js/task5.js
@@ -0,0 +1,14 @@
+'user strict'
+function checkForSpam(str) {
+  return (
+    str.toLowerCase().includes('spam') || str.toLowerCase().includes('sale')
+  );
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
